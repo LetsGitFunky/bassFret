396be0eacfec7124a7e01ceeb8f2c04c938633d3
LetsGitFunky <NickHeinBass@gmail.com>
2023-09-12 14:12:50 -0700

    webpack config updated with mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    contentBase: './dist',
-    open: true
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        static: './dist',
+        open: true,
+        port: 3000
+    }
 };
